2f84d93010b7b8ff636a718ec5fafa4552b2553c
nicolic0 <nickolasm@saintjamesacademy.com>
2024-11-01 19:10:43 +0000

    reviewing stuff

--- lessons/00_Turtles/10a_Tash_Me.py
+++ lessons/00_Turtles/10a_Tash_Me.py
@@ -11,3 +11,24 @@ Hint: See 08a_More Turtle Programs, section 'Change the Background Image' and
 
 ... # Your code here
 
+import PIL  
+import turtle
+screen= turtle.Screen()
+def set_backround_image(window, image_name):
+    from pathlib import Path
+    from PIL import Image
+
+
+    image_dir = Path(__file__).parent / "images"
+    image_path = str(image_dir / image_name)
+
+    image = Image.open(image_path)
+    
+    window.setup(image.width, image.height, startx=0, starty=0)
+    window.bgpic(image_path)
+set_backround_image(screen, "emoji.png")
+
+    
+
+turtle.exitonclick()
+
